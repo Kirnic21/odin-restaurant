23d3d8951fce99dfc43088dac92df00f8a8e58c0
Kirnic21 <marcelokenji12121@gmail.com>
2023-02-27 20:46:46 -0300

    Done main.js logic for buttons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
 import './style.css';
 import {mainPage} from './page1'
 import {menu} from './page2'
-//mainPage()
-//menu()
+import {contacts} from "./page3"
+let page = 0;
+const mainpage = document.querySelector("#mainpage")
+const menupage = document.querySelector("#menupage")
+const contactpage = document.querySelector("#contactpage")
+const contents = document.querySelector("#content")
+mainPage()
+menupage.addEventListener("click",function mainpage1(){
+    if(page!==1)
+    {
+    page=1;
+    contents.innerHTML=" "
+    menu()
+    console.log(page)
+    }
+})
+mainpage.addEventListener("click",function mainpage0()
+{
+    if(page!==0)
+    {
+        page=0;
+        contents.innerHTML=" "
+        mainPage()
+        console.log(page)
+    }
+});
+contactpage.addEventListener("click",function mainpage2(){
+    if (page!==2)
+    {
+        page = 2;
+        contents.innerHTML = " "
+        contacts()
+        console.log(page)
+    }
+})
--- src/page3.js
+++ src/page3.js
@@ -11,6 +11,7 @@ content.appendChild(container)
 const contact = document.createElement("div")
 contact.classList.add("Contact")
 contact.textContent = "Remilia Scarlet"
+container.appendChild(contact)
 const ulDesc = document.createElement("ul")
 ulDesc.setAttribute("id","desc")
 contact.appendChild(ulDesc)
@@ -20,6 +21,32 @@ li1.textContent = "Number: 69696-420"
 ulDesc.appendChild(li1)
 const li2 = document.createElement("li")
 li2.classList.add("descli")
-li2.textContent = ""
+li2.textContent = "Local:Scarlet Devil Mansion"
 ulDesc.appendChild(li2)
+const contact2 = document.createElement("div")
+contact2.textContent ="Flandre Scarlet"
+contact2.classList.add("Contact")
+container.appendChild(contact2)
+const ulDesc2 = document.createElement("ul")
+ulDesc2.setAttribute("id","desc")
+contact2.appendChild(ulDesc2)
+const li3 = document.createElement("li")
+li3.classList.add("descli")
+li3.textContent = "Number: 420-6969"
+ulDesc2.appendChild(li3)
+const li4 = document.createElement("li")
+li4.classList.add("descli")
+li4.textContent = "Local:Scarlet Devil Mansion Basement"
+ulDesc2.appendChild(li4);
+const location = document.createElement("div")
+location.classList.add("Location")
+container.appendChild(location)
+const loctitle = document.createElement("div")
+loctitle.classList.add("Loc-Title")
+loctitle.textContent="Our Location: "
+location.appendChild(loctitle)
+const img = document.createElement("img");
+img.classList.add("LocMap")
+img.src = "Screenshot_1.png";
+location.appendChild(img)
 }
